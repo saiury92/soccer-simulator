a6fbb7e17658300a0ab07e2116e289df220d56bc
saiury92 <khanhthien2105@gmail.com>
2021-09-22 19:18:52 +0700

    update feature ballAction

--- public/js/match.js
+++ public/js/match.js
@@ -1,6 +1,6 @@
 // User Settings
-var gamelength = 10000;
-var speed = 20;
+var gamelength = 50;
+var speed = 1500;
 // -----------------------
 var matchInfo;
 var its = 0;
@@ -18,98 +18,7 @@ function setPositions() {
   http.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
       var result = JSON.parse(this.responseText);
-
-      var c = document.getElementById("map");
-      var ctx = c.getContext("2d");
-
-      for (var position in result.kickOffTeamGroupByPosition) {
-        ctx.beginPath();
-        ctx.fillStyle = 'black';
-        var shortName = ''
-        for (player of result.kickOffTeamGroupByPosition[position]) {
-          shortName = shortName + ' ' + player.shortName
-        }
-        switch (position) {
-          case 'GK':
-            ctx.fillText(shortName, 250, 60)
-            break;
-          case 'RB':
-            ctx.fillText(shortName, 15, 150)
-            break;
-          case 'CB':
-            ctx.fillText(shortName, 210, 150)
-            break;
-          case 'LB':
-            ctx.fillText(shortName, 405, 150)
-            break;
-          case 'RM':
-            ctx.fillText(shortName, 15, 400)
-            break;
-          case 'CM':
-            ctx.fillText(shortName, 210, 400)
-            break;
-          case 'LM':
-            ctx.fillText(shortName, 405, 400)
-            break;
-          case 'RWF':
-            ctx.fillText(shortName, 15, 650)
-            break;
-          case 'ST':
-            ctx.fillText(shortName, 210, 650)
-            break;
-          case 'LWF':
-            ctx.fillText(shortName, 405, 650)
-            break;
-        }
-      }
-
-      for (var position in result.secondTeamGroupByPosition) {
-        ctx.beginPath();
-        ctx.fillStyle = 'black';
-        var shortName = ''
-        for (player of result.secondTeamGroupByPosition[position]) {
-          shortName = shortName + ' ' + player.shortName
-        }
-        switch (position) {
-          case 'GK':
-            ctx.fillText(shortName, 250, 790)
-            break;
-          case 'RB':
-            ctx.fillText(shortName, 405, 700)
-            break;
-          case 'CB':
-            ctx.fillText(shortName, 210, 700)
-            break;
-          case 'LB':
-            ctx.fillText(shortName, 15, 700)
-            break;
-          case 'RM':
-            ctx.fillText(shortName, 405, 450)
-            break;
-          case 'CM':
-            ctx.fillText(shortName, 210, 450)
-            break;
-          case 'LM':
-            ctx.fillText(shortName, 15, 450)
-            break;
-          case 'RWF':
-            ctx.fillText(shortName, 405, 200)
-            break;
-          case 'ST':
-            ctx.fillText(shortName, 210, 200)
-            break;
-          case 'LWF':
-            ctx.fillText(shortName, 15, 200)
-            break;
-        }
-      }
-
-      var ballPixels = ballCoordinatesToPixels(...result.ball.position, "top")
-      ctx.beginPath();
-      ctx.arc(...ballPixels, 15, 0, Math.PI * 2);
-      ctx.fill();
-
-      document.getElementById("result").innerHTML = result.kickOffTeam.name + ": " + result.kickOffTeamStatistics.goals + " - " + result.secondTeamStatistics.goals + " :" + result.secondTeam.name;
+      setupPitch(result)
     }
   };
   http.open("GET", "/getstartPOS", true);
@@ -122,19 +31,19 @@ function ballCoordinatesToPixels(x, y, side) {
       case 0:
         return [300, 35]
       default:
-        return [15 + x * 15 + x * 180 + 90, 100 + 250 * (y-1) + 25]
+        return [15 + x * 15 + x * 180 + 90, 100 + 250 * (y - 1) + 25]
     }
   } else {
     switch (y) {
       case 4:
         return [300, 765]
       default:
-        return [15 + x * 15 + x * 180 + 90, 150 + 250 * (y-1) + 25]
+        return [15 + x * 15 + x * 180 + 90, 150 + 250 * (y - 1) + 25]
     }
   }
 }
 
-function setupPitch() {
+function setupPitch(result) {
   var c = document.getElementById("map");
   var ctx = c.getContext("2d");
   ctx.canvas.width = 600;
@@ -179,6 +88,104 @@ function setupPitch() {
   ctx.fillRect(250, 740, 100, 50);
 
   ctx.stroke();
+
+  if (!result) return;
+
+  for (var position in result.kickOffTeamGroupByPosition) {
+    ctx.beginPath();
+    ctx.fillStyle = 'black';
+    var shortName = ''
+    for (player of result.kickOffTeamGroupByPosition[position]) {
+      shortName = shortName + ' ' + player.shortName
+    }
+    switch (position) {
+      case 'GK':
+        ctx.fillText(shortName, 250, 60)
+        break;
+      case 'RB':
+        ctx.fillText(shortName, 15, 150)
+        break;
+      case 'CB':
+        ctx.fillText(shortName, 210, 150)
+        break;
+      case 'LB':
+        ctx.fillText(shortName, 405, 150)
+        break;
+      case 'RM':
+        ctx.fillText(shortName, 15, 400)
+        break;
+      case 'CM':
+        ctx.fillText(shortName, 210, 400)
+        break;
+      case 'LM':
+        ctx.fillText(shortName, 405, 400)
+        break;
+      case 'RWF':
+        ctx.fillText(shortName, 15, 650)
+        break;
+      case 'ST':
+        ctx.fillText(shortName, 210, 650)
+        break;
+      case 'LWF':
+        ctx.fillText(shortName, 405, 650)
+        break;
+    }
+  }
+
+  for (var position in result.secondTeamGroupByPosition) {
+    ctx.beginPath();
+    ctx.fillStyle = 'black';
+    var shortName = ''
+    for (player of result.secondTeamGroupByPosition[position]) {
+      shortName = shortName + ' ' + player.shortName
+    }
+    switch (position) {
+      case 'GK':
+        ctx.fillText(shortName, 250, 790)
+        break;
+      case 'RB':
+        ctx.fillText(shortName, 405, 700)
+        break;
+      case 'CB':
+        ctx.fillText(shortName, 210, 700)
+        break;
+      case 'LB':
+        ctx.fillText(shortName, 15, 700)
+        break;
+      case 'RM':
+        ctx.fillText(shortName, 405, 450)
+        break;
+      case 'CM':
+        ctx.fillText(shortName, 210, 450)
+        break;
+      case 'LM':
+        ctx.fillText(shortName, 15, 450)
+        break;
+      case 'RWF':
+        ctx.fillText(shortName, 405, 200)
+        break;
+      case 'ST':
+        ctx.fillText(shortName, 210, 200)
+        break;
+      case 'LWF':
+        ctx.fillText(shortName, 15, 200)
+        break;
+    }
+  }
+
+  var side = (result.kickOffTeam.teamID === result.ball.withTeam) ? "top" : "bottom"
+  var ballPixels = ballCoordinatesToPixels(...result.ball.position, side)
+  ctx.beginPath();
+  ctx.fillStyle = "#F00000";
+  ctx.arc(...ballPixels, 15, 0, Math.PI * 2);
+  ctx.fill();
+  ctx.stroke();
+
+  document.getElementById("result").innerHTML = result.kickOffTeam.name + ": " + result.kickOffTeamStatistics.goals + " - " + result.secondTeamStatistics.goals + " :" + result.secondTeam.name;
+  logs += "Iteration " + its + ": " + result.iterationLog + "<br>";
+  document.getElementById("logging").innerHTML = logs;
+  var elem = document.getElementById('logging');
+  elem.scrollTop = elem.scrollHeight;
 }
 
 function pauseGame() {
@@ -208,7 +215,7 @@ function moveBall(endpoint) {
     if (this.readyState === 4 && this.status === 200) {
       its++;
       var result = JSON.parse(this.responseText);
-      console.log(result);
+      setupPitch(result)
     }
   };
   http.open("GET", endpoint, true);
